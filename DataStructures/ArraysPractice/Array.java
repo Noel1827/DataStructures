4b086ee70e62d788cbe65821ed4a5b49e2e51278
Noel1827 <noel.vargas5@upr.edu>
2021-02-22 23:20:45 -0400

    IndexOf function

--- DataStructures/ArraysPractice/Array.java
+++ DataStructures/ArraysPractice/Array.java
@@ -24,4 +24,24 @@ public class Array {
         items[count++]= item;
     }
 
+    public void removeAt(int index){
+        if(index >= count || index <0)
+        throw new IllegalArgumentException("Index out of bounds");
+
+        for(int i= index; i < count; i++){
+            items[i] = items[i+1];
+        }
+        count--;
+    }
+
+    public int indexOf(int item){
+        // if it finds the item, return index
+        // otherwise, return -1
+        for(int i=0; i<count; i++){
+            if(items[i] == item)
+            return i;
+        }
+        return -1;
+    }
+
 }
