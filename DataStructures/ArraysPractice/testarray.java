65d20c7c9fbaa75fbec8d833f57bf696664f27a8
Noel A Vargas Padilla <70292282+Noel1827@users.noreply.github.com>
2021-02-22 22:02:42 -0400

    Add files via upload
    Initial Code

---
+++ DataStructures/ArraysPractice/testarray.java
@@ -0,0 +1,11 @@
+public class testarray {
+    public static void main(String args[]){
+       NoelArray b = new NoelArray(0);
+       b.insert(4);
+       b.insert(1);
+    b.insert(2);
+    b.removeAt(0);
+       b.print();
+        
+    }
+}
---
+++ DataStructures/Data Structures - Part 1/DataStructures/src/com/codewithmosh/Main.java
@@ -0,0 +1,13 @@
+package com.codewithmosh;
+
+public class Main {
+    public static void main(String[] names) {
+        Array numbers = new Array(3);
+     numbers.insert(10);
+     numbers.insert(20);
+     numbers.insert(30);
+     numbers.insert(40);
+     numbers.removeAt(3);
+     System.out.println(numbers.indexOf(10));
+    }
+}
