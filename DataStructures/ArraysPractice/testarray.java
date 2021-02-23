b550690757793e8e2c0d88ac7990cfab9980a024
Noel1827 <noel.vargas5@upr.edu>
2021-02-22 22:12:26 -0400

    java Array solution

--- DataStructures/ArraysPractice/testarray.java
+++ DataStructures/ArraysPractice/testarray.java
@@ -1,11 +1,6 @@
 public class testarray {
     public static void main(String args[]){
-       NoelArray b = new NoelArray(0);
-       b.insert(4);
-       b.insert(1);
-    b.insert(2);
-    b.removeAt(0);
-       b.print();
-        
+      Array numbers = new Array(3);
+        numbers.print();
     }
 }
