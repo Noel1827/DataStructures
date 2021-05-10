9f8aaea9137098f75cb5b2517dbc76bd7658dcb0
Noel1827 <noel.vargas5@upr.edu>
2021-05-10 11:26:16 -0400

    done

---
+++ DataStructures/Data Structures - Part 1/DataStructures/src/com/codewithmosh/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int i;
+        for (i = 0; i < 25; i++)
+        {
+            cout << ".|. \t";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
